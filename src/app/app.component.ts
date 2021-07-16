a5880334f32e755b16d283b9d6676f7ad4db1ed2
Sivaranjini <Sivaranjini@HTCLT516.local>
2021-07-16 19:29:43 +0530

    angular july16 changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-baatch1';
+
+
+  clickButton(val){
+    alert(val)
+  }
+
+  myEvent(val){
+    console.log(val)
+  }
+  course="Angular";
+  isDisabled=true;
+
+  enableText(){
+    this.isDisabled = false;
+  }
+
+  show=true;
+  color='green'
+
+  colorSwitch=''
+
+  data=[
+    {
+      name:'xxx',
+      age:'23'
+    },
+    {
+      name:'yyy',
+      age:'24'
+    }
+  ]
+
 }
